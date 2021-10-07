51bc61b665ca11e1f7b4ae7d51c1264d4545a092
Mikel Adkins <dev.mikeladkins@gmail.com>
2021-10-06 20:32:13 -0400

    Adding dungeon affixes enum and icons

--- app/src/main/java/com/madkins/completoandroid/data/DungeonAffixes.kt
+++ app/src/main/java/com/madkins/completoandroid/data/DungeonAffixes.kt
@@ -1,5 +1,18 @@
 package com.madkins.completoandroid.data
 
-enum class DungeonAffixes(val affixName: String, val iconId: Int) {
+import com.madkins.completoandroid.R
 
+enum class DungeonAffixes(val affixName: String, val iconId: Int) {
+    BOLSTERING("Bolstering", R.drawable.affix_bolstering),
+    BURSTING("Bursting", R.drawable.affix_bursting),
+    FORTIFIED("Fortified", R.drawable.affix_fortified),
+    GRIEVOUS("Grievous", R.drawable.affix_grievous),
+    INSPIRING("Inspiring", R.drawable.affix_inspiring),
+    NECROTIC("Necrotic", R.drawable.affix_necrotic),
+    QUAKING("Quaking", R.drawable.affix_quaking),
+    SPITEFUL("Spiteful", R.drawable.affix_spiteful),
+    STORMING("Storming", R.drawable.affix_storming),
+    TORMENTED("Tormented", R.drawable.affix_tormented),
+    TYRANNICAL("Tyrannical", R.drawable.affix_tyrannical),
+    VOLCANIC("Volcanic", R.drawable.affix_volcanic)
 }
